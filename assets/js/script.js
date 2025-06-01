93246968f8a49ff762d6b17a75f0880b0eccf9f7
SakshiK91 <103639117+SakshiK91@users.noreply.github.com>
2025-06-01 13:32:37 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -95,3 +95,15 @@ function applyTypewriterEffect(element, text) {
   setTimeout(typeCharacter, 500);
 }
 
+document.addEventListener("DOMContentLoaded",function(){
+  var navLinks= documnet.querySelectorAll('.navbar-nav .nav-link');
+  var navbarCollapse=document.querySelector('.navbar-collapse');
+  navLinks.forEach(function(link){
+    link.addEventListener('click',function(){
+      if(navbarCollapse.classList.contains('show')){
+        var bsCollapse = new bootstrap.Collapse(navbarCollapse ,{toggle:false});
+        bsCollapse.hide();
+      }
+    });
+  });
+});
